d2f04e1ed7465a5001d04434cd21d5a964bd4b14
chen zhou <imjonezhou@gmail.com>
2020-10-29 18:28:42 +0800

    init

---
+++ install.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+
+# brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew install
+brew install git
+brew install tree
+brew install autojump
+brew install ack
+brew install wget
+
+echo '[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh' >> ~/.zshrc
+source ~/.zshrc
+
+# vim 
+curl -L https://bit.ly/janus-bootstrap | bash
+
+# prepare folders
+mkdir -p ~/workspace
+
+# node version manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
+source ~/.zshrc
+
+nvm install stable
+nvm alias default node 
+
+npm install --global yrm --registry=https://registry.npm.taobao.org
+
+yrm use cnpm
+npm i -g yarn
+
+# https://github.com/i5ting/i5ting-mac-init/issues/1 
+# ~/.oh-my-zsh/plugins/git/git.plugin.zsh 和lazycommit效果一样，故而弃用
+npm i -g lazycommit
+npm i -g lazyclone
+
+ # ssh-keygen 
+ssh-keygen 
+cat ~/.ssh/id_rsa.pub
+echo 'save it to gitlab or https://github.com/settings/keys'
